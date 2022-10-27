76ce5ae06bb95781e0f5f22d2161b8d96e3781fe
caio-mitz <caioschmitz09@gmail.com>
2022-10-27 15:13:13 -0300

    progresso na confecção da ficha de personagem

--- src/views/FichaOrchid.vue
+++ src/views/FichaOrchid.vue
@@ -1,160 +1,153 @@
 <template>
+  <div class="ficha-orchid">
+    <section class="info-1">
+      <!-- FOTO DO PERSONAGEM -->
+      <div class="pos-foto">
+        <div class="top-box foto-pers">
+          <!-- FOTO DO PERSONAGEM -->
+          a
+        </div>
+      </div>
+      <!-- FIM DA FOTO DO PERSONAGEM -->
 
-  	<div class="ficha-orchid">
-
-    	<section class="info-1">
-
-      	<!-- FOTO DO PERSONAGEM -->
-      		<div class="pos-foto">
-        		<div class="top-box foto-pers">
-          			<!-- FOTO DO PERSONAGEM -->
-         			 a
-        		</div>
-      		</div>
-		<!-- FIM DA FOTO DO PERSONAGEM -->
-
-      	<section class="sub-info-1">
-
+      <section class="sub-info-1">
         <!-- INPUTS DOS STATS EM NÚMEROS -->
-        	<div class="top-box nex-pers">
-          		<!-- INPUT NEX DO PERSONAGEM -->
-         		<h1>NEX</h1>
-          		<input type="text" class="input-nex">
-        	</div>
-        	<div class="top-box perodada-pers">
-          		<!-- INPUT PE DO PERSONAGEM -->
-          		<h1>PE/RODADA</h1>
-          		<input type="text" class="input-perodada">
-        	</div>
-        	<div class="top-box desl-pers">
-          		<!-- INPUT DESLOCAMENTO DO PERSONAGEM -->
-          		<h1>DESL.</h1>
-          		<input type="text" class="input-deslocamento">
-        	</div>
-        	<div class="top-box quad-pers">
-				<h1>QUAD.</h1>
-				<input type="text" class="input-quadrados">
-          		<!-- INPUT QUADRADOS DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box pvtotal-pers">
-          		<h1>PV TOTAL</h1>
-          		<input type="text" class="input-pvtotal">
-          		<!-- INPUT PV TOTAL DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box pvatual-pers">
-				<h1>PV ATUAL</h1>
-				<input type="text" class="input-pvatual">
-          		<!-- INPUT PV ATUAL DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box santotal-pers">
-          		<h1>SAN TOTAL</h1>
-          		<input type="text" class="input-santotal">
-          		<!-- INPUT SAN TOTAL DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box sanatual-pers">
-				<h1>SAN ATUAL</h1>
-				<input type="text" class="input-sanatual">
-          		<!-- INPUT SAN ATUAL DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box petotal-pers">
-          		<h1>PE TOTAL</h1>
-          		<input type="text" class="input-petotal">
-          		<!-- INPUT PE TOTAL DO PERSONAGEM -->
-        	</div>
-        	<div class="top-box peatual-pers">
-				<h1>PE ATUAL</h1>
-				<input type="text" class="input-peatual">
-          		<!-- INPUT PE ATUAL DO PERSONAGEM -->
-        	</div>
+        <div class="top-box nex-pers">
+          <!-- INPUT NEX DO PERSONAGEM -->
+          <h1>NEX</h1>
+          <input type="text" class="input-nex" />
+        </div>
+        <div class="top-box perodada-pers">
+          <!-- INPUT PE DO PERSONAGEM -->
+          <h1>PE/RODADA</h1>
+          <input type="text" class="input-perodada" />
+        </div>
+        <div class="top-box desl-pers">
+          <!-- INPUT DESLOCAMENTO DO PERSONAGEM -->
+          <h1>DESL.</h1>
+          <input type="text" class="input-deslocamento" />
+        </div>
+        <div class="top-box quad-pers">
+          <h1>QUAD.</h1>
+          <input type="text" class="input-quadrados" />
+          <!-- INPUT QUADRADOS DO PERSONAGEM -->
+        </div>
+        <div class="top-box pvtotal-pers">
+          <h1>PV TOTAL</h1>
+          <input type="text" class="input-pvtotal" />
+          <!-- INPUT PV TOTAL DO PERSONAGEM -->
+        </div>
+        <div class="top-box pvatual-pers">
+          <h1>PV ATUAL</h1>
+          <input type="text" class="input-pvatual" />
+          <!-- INPUT PV ATUAL DO PERSONAGEM -->
+        </div>
+        <div class="top-box santotal-pers">
+          <h1>SAN TOTAL</h1>
+          <input type="text" class="input-santotal" />
+          <!-- INPUT SAN TOTAL DO PERSONAGEM -->
+        </div>
+        <div class="top-box sanatual-pers">
+          <h1>SAN ATUAL</h1>
+          <input type="text" class="input-sanatual" />
+          <!-- INPUT SAN ATUAL DO PERSONAGEM -->
+        </div>
+        <div class="top-box petotal-pers">
+          <h1>PE TOTAL</h1>
+          <input type="text" class="input-petotal" />
+          <!-- INPUT PE TOTAL DO PERSONAGEM -->
+        </div>
+        <div class="top-box peatual-pers">
+          <h1>PE ATUAL</h1>
+          <input type="text" class="input-peatual" />
+          <!-- INPUT PE ATUAL DO PERSONAGEM -->
+        </div>
         <!-- FIM DOS INPUTS DOS STATS EM NÚMEROS -->
-
       </section>
     </section>
 
     <section class="info-2">
-    	<section class="sub-info-2">
-        	<!-- INPUTS INICIAIS -->
-        	<div class="top-box nome-pers">
-          		<h1>NOME DO PERSONAGEM:</h1>
-          		<input type="text" class="input-nome">
-        	</div>
-        	<div class="top-box classe-pers">
-          		<h1>CLASSE DO PERSONAGEM:</h1>
-          		<input type="text" class="input-classe">
-        	</div>
-        	<div class="top-box jogador-pers">
-          		<h1>NOME DO JOGADOR:</h1>
-          		<input type="text" class="input-jogador">
-        	</div>
-        	<div class="top-box origem-pers">
-         		<h1>ORIGEM DO PERSONAGEM:</h1>
-         		<input type="text" class="input-origem">
-        	</div>
+      <section class="sub-info-2">
+        <!-- INPUTS INICIAIS -->
+        <div class="top-box nome-pers">
+          <h1>NOME DO PERSONAGEM:</h1>
+          <input type="text" class="input-nome" />
+        </div>
+        <div class="top-box classe-pers">
+          <h1>CLASSE DO PERSONAGEM:</h1>
+          <input type="text" class="input-classe" />
+        </div>
+        <div class="top-box jogador-pers">
+          <h1>NOME DO JOGADOR:</h1>
+          <input type="text" class="input-jogador" />
+        </div>
+        <div class="top-box origem-pers">
+          <h1>ORIGEM DO PERSONAGEM:</h1>
+          <input type="text" class="input-origem" />
+        </div>
         <!-- FIM DOS INPUTS INICIAIS -->
 
         <!-- CAIXAS DE INFORMAÇÕES -->
-        	<div class="itens-pers">
-          		<!-- ITENS -->
-          		a
-        	</div>
-        	<div class="proef-pers">
-          		<!-- PROEFICIÊNCIAS E OUTROS -->
-          		a
-        	</div>
-        	<div class="habil-pers">
-          		<!-- HABILIDADES, RITUAIS E PODERES -->
-          		a
-        	</div>
+        <div class="itens-pers">
+          <!-- ITENS -->
+          a
+        </div>
+        <div class="proef-pers">
+          <!-- PROEFICIÊNCIAS E OUTROS -->
+          a
+        </div>
+        <div class="habil-pers">
+          <!-- HABILIDADES, RITUAIS E PODERES -->
+          a
+        </div>
 
         <!-- CAIXAS DE INFORMAÇÕES -->
         <!-- HEXÁGONOS -->
 
-        	<div class="hex1">
-          		<h1>FOR</h1>
-          		<div class="hex1-pers">
-            		<!-- HEXÁGONO -->
-          		</div>
-        	</div>
+        <div class="hex1">
+          <h1>FOR</h1>
+          <div class="hex1-pers">
+            <input type="text" />
+          </div>
+        </div>
 
-        	<div class="hex2">
-          		<h1>AGI</h1>
-          		<div class="hex2-pers">
-           			<!-- HEXÁGONO -->
-          		</div>
-        	</div>
+        <div class="hex2">
+          <h1>AGI</h1>
+          <div class="hex2-pers">
+            <input type="text" />
+          </div>
+        </div>
 
-        	<div class="hex3">
-         		<h1>INT</h1>
-          		<div class="hex3-pers">
-            		<!-- HEXÁGONO -->
-          		</div>
-        	</div>
+        <div class="hex3">
+          <h1>INT</h1>
+          <div class="hex3-pers">
+            <input type="text" />
+          </div>
+        </div>
 
-        	<div class="hex4">
-          		<h1>VIG</h1>
-          		<div class="hex4-pers">
-            		<!-- HEXÁGONO -->
-          		</div>
-       	 	</div>
+        <div class="hex4">
+          <h1>VIG</h1>
+          <div class="hex4-pers">
+            <input type="text" />
+          </div>
+        </div>
 
-        	<div class="hex5">
-          		<h1>PRE</h1>
-          		<div class="hex5-pers">
-            		<!-- HEXÁGONO -->
-          		</div>
-        	</div>
-
-      	</section>
+        <div class="hex5">
+          <h1>PRE</h1>
+          <div class="hex5-pers">
+            <input type="text" />
+          </div>
+        </div>
+      </section>
     </section>
 
     <section class="info-3">
-      	<section class="sub-info-3">
-        	<div class="top-box logo-ordem">
-          		<img src="@/assets/Ordem_Paranormal_Logo.png" alt="">
-        	</div>
+      <section class="sub-info-3">
+        <div class="top-box logo-ordem">
+          <img src="@/assets/Ordem_Paranormal_Logo.png" alt="" />
+        </div>
         <div class="pericias-pers">
-          	a
-          	<!-- PERICIAS -->
+          <!-- PERICIAS -->
         </div>
       </section>
     </section>
@@ -166,5 +159,5 @@ export default {};
 </script>
 
 <style scoped>
-  @import '@/assets/styles/fichaorchid.css';
+@import "@/assets/styles/fichaorchid.css";
 </style>
