3f04ecf3e2ef95a03c8d054ae8e06b2994ed5629
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-12-04 12:09:57 -0300

    Botão criar ficha e display flex fichas

--- src/views/MinhasFichas.vue
+++ src/views/MinhasFichas.vue
@@ -1,103 +1,109 @@
 <template>
-    <div class="minhas-fichas">
-        <div class="col1">
-            <div class="conteudo1">
-                <div class="ficha1">
-                    <div class="cont-1">
-                        <div class="foto1"></div>
-                    </div>
-                    <div class="cont-2">
-                        <div class="desc1">
-                            <h1 class="titulo">MINHA FICHA</h1>
-                            <div class="det1">
-                                <h1 class="det-nex-classe">NEX</h1>
-                                <h1 class="det-nex-classe">CLASSE</h1>
-                            </div>
-                            <div class="det2">
-                                <h1 class="detnum">N</h1>
-                                <h1 class="detnum">N</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div>
+        <div class="minhas-fichas">
+            <div class="criar-button" @click="$router.push({path: '/criarficha'})">
+                <PlusIcon/>
+                <div>CRIAR FICHA</div>
             </div>
-        </div>
-        <div class="col2">
-            <div class="conteudo1">
-                <div class="ficha1">
-                    <div class="cont-1">
-                        <div class="foto1"></div>
-                    </div>
-                    <div class="cont-2">
-                        <div class="desc1">
-                            <h1 class="titulo">MINHA FICHA</h1>
-                            <div class="det1">
-                                <h1 class="det-nex-classe">NEX</h1>
-                                <h1 class="det-nex-classe">CLASSE</h1>
-                            </div>
-                            <div class="det2">
-                                <h1 class="detnum">N</h1>
-                                <h1 class="detnum">N</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col3">
-            <div class="conteudo1">
-                <div class="ficha1">
-                    <div class="cont-1">
-                        <div class="foto1"></div>
-                    </div>
-                    <div class="cont-2">
-                        <div class="desc1">
-                            <h1 class="titulo">MINHA FICHA</h1>
-                            <div class="det1">
-                                <h1 class="det-nex-classe">NEX</h1>
-                                <h1 class="det-nex-classe">CLASSE</h1>
-                            </div>
-                            <div class="det2">
-                                <h1 class="detnum">N</h1>
-                                <h1 class="detnum">N</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col4">
-            <div class="conteudo1">
-                <div class="ficha1">
-                    <div class="cont-1">
-                        <div class="foto1"></div>
-                    </div>
-                    <div class="cont-2">
-                        <div class="desc1">
-                            <h1 class="titulo">MINHA FICHA</h1>
-                            <div class="det1">
-                                <h1 class="det-nex-classe">NEX</h1>
-                                <h1 class="det-nex-classe">CLASSE</h1>
-                            </div>
-                            <div class="det2">
-                                <h1 class="detnum">N</h1>
-                                <h1 class="detnum">N</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <div class="cards">
 
-        <div class="col2"></div>
-        <div class="col3"></div>
-        <div class="col4"></div>
+                <div class="conteudo1">
+                    <div class="ficha1">
+                        <div class="cont-1">
+                            <div class="foto1"></div>
+                        </div>
+                        <div class="cont-2">
+                            <div class="desc1">
+                                <h1 class="titulo">MINHA FICHA</h1>
+                                <div class="det1">
+                                    <h1 class="det-nex-classe">NEX</h1>
+                                    <h1 class="det-nex-classe">CLASSE</h1>
+                                </div>
+                                <div class="det2">
+                                    <h1 class="detnum">N</h1>
+                                    <h1 class="detnum">N</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            
+                <div class="conteudo1">
+                    <div class="ficha1">
+                        <div class="cont-1">
+                            <div class="foto1"></div>
+                        </div>
+                        <div class="cont-2">
+                            <div class="desc1">
+                                <h1 class="titulo">MINHA FICHA</h1>
+                                <div class="det1">
+                                    <h1 class="det-nex-classe">NEX</h1>
+                                    <h1 class="det-nex-classe">CLASSE</h1>
+                                </div>
+                                <div class="det2">
+                                    <h1 class="detnum">N</h1>
+                                    <h1 class="detnum">N</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            
+                <div class="conteudo1">
+                    <div class="ficha1">
+                        <div class="cont-1">
+                            <div class="foto1"></div>
+                        </div>
+                        <div class="cont-2">
+                            <div class="desc1">
+                                <h1 class="titulo">MINHA FICHA</h1>
+                                <div class="det1">
+                                    <h1 class="det-nex-classe">NEX</h1>
+                                    <h1 class="det-nex-classe">CLASSE</h1>
+                                </div>
+                                <div class="det2">
+                                    <h1 class="detnum">N</h1>
+                                    <h1 class="detnum">N</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            
+                <div class="conteudo1">
+                    <div class="ficha1">
+                        <div class="cont-1">
+                            <div class="foto1"></div>
+                        </div>
+                        <div class="cont-2">
+                            <div class="desc1">
+                                <h1 class="titulo">MINHA FICHA</h1>
+                                <div class="det1">
+                                    <h1 class="det-nex-classe">NEX</h1>
+                                    <h1 class="det-nex-classe">CLASSE</h1>
+                                </div>
+                                <div class="det2">
+                                    <h1 class="detnum">N</h1>
+                                    <h1 class="detnum">N</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import PlusIcon from 'vue-material-design-icons/Plus.vue';
 export default {
-    
+    components:{
+        PlusIcon
+    }
 }
 </script>
 
